af4dc4ce82109ea54ba84456672766ba41d455ac
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-12-07 22:28:42 -0500

    create locations.js

---
+++ frontend/src/util/locations.js
@@ -0,0 +1,23 @@
+export default locations = {
+    battleFactory: {
+        name: "Battle Factory",
+    },
+    battleArena: {
+        name: "Battle Arena",
+    },
+    battleDome: {
+        name: "Battle Dome",
+    },
+    battlePike: {
+        name: "Battle Pike",
+    },
+    battlePalace: {
+        name: "Battle Palace"
+    },
+    battlePyramid: {
+        name: "Battle Pyramid",
+    },
+    battleTower: {
+        name: "Battle Tower"
+    }
+}
