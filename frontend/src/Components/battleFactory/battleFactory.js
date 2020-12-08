5286c1904fcf3d6161d2966f7a077b4406ec4126
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-12-07 22:40:06 -0500

    create Battle Factory

---
+++ frontend/src/Components/battleFactory/battleFactory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const battleFactory = () => {
+    return (
+        <main className="bf-container">
+            <h1>Battle Factory</h1>
+        </main>
+    )
+}
+
+export default battleFactory;
