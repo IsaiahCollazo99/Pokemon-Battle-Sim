66fad0c47e8242bbadb3bf5a3a29002c5139ea29
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-12-07 22:07:54 -0500

    Home and Landing components

---
+++ frontend/src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <main className="homeContainer">
+            
+        </main>
+    )
+}
+
+export default Home;
---
+++ frontend/src/Components/Home/LocationCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LocationCard = ( location ) => {
+    return (
+        <article className="locationCard">
+            
+        </article>
+    )
+}
+
+export default LocationCard
